48455f8a58dc2b7ccabfe7e2984812f82b556b9a
U-ALTAIR\lars <lars@ALTAIR.(none)>
2009-05-25 23:10:39 -0700

    Build 100: * First iteration of the R2 test suite. * New voice recordings. * No foot-tapping test yet. * Same KDClient.exe and KineticsControl.dll as Build 54c, but significant changes were made to the JavaScript/HTML layer to account for changes specified by the R2 requirements.

--- root/Device Software/HTML/constants.js
+++ root/Device Software/HTML/constants.js
@@ -11,22 +11,31 @@ INACTIVITY_TIMEOUT			= 60 * 60 * 1000;
 
 // length of time to complete one round of the reaction or other untimed test.  (msecs)
 TEST_TIMEOUT				= 2 * 60 * 1000;
+//TEST_TIMEOUT                = 60;
 // length of time to complete one round of the peg test
 PEG_TIMEOUT					= TEST_TIMEOUT;
 // length of time to complete one round of the reaction test
 REACTION_TIMEOUT			= TEST_TIMEOUT;
 // length of time to complete one round of the tap test
-TAP_TIMEOUT					= 20000;
+TAP_TIMEOUT					= 60000;
+//TAP_TIMEOUT                 = 60;
 // length of time to complete one round of the keyboard test
-KEY_TIMEOUT					= 30000;
+KEY_TIMEOUT					= 60000;
+//KEY_TIMEOUT                 = 60;
 
-// fixed time to say "ahh" (in SECONDS)
-VOICE_AHH_FIXED_TIMEOUT		= 35;
-// fixed time to describe picture (in SECONDS)
-VOICE_TALK_FIXED_TIMEOUT	= 30;
+// fixed time to say "ahh" (in SECONDS), also used for DDK tests.
+VOICE_AHH_FIXED_TIMEOUT		= 12;
+// fixed time to read first two passages (in SECONDS)
+VOICE_TALK_FIXED_TIMEOUT	= 50;
+// fixed time to read last passage (in SECONDS)
+VOICE_TALK_FIXED_TIMEOUT_2    = 60;
+//VOICE_TALK_FIXED_TIMEOUT    = 10;
+// length of time to complete one round of the foot tapping test (in SECONDS)
+FOOT_TIMEOUT                = 45;
+//FOOT_TIMEOUT                = 10;
 
 // length of time in between activities (msecs)
-ACTIVITY_SPACING_TIME		= 5000;
+ACTIVITY_SPACING_TIME		= 2000;
 // time to wait for "get ready" audio to play (msecs)
 GET_READY_DELAY				= 3000;
 // time to wait for "wait for the tone get ready" audio to play (msecs)
